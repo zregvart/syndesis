8e36c69860df5ae718567be71db6d26d83c6d2b7
Zoran Regvart <zregvart@apache.org>
2017-12-08 16:57:42 +0100

    Hacky fix

--- app/connectors/connectors/sql/sql-start-connector/src/main/java/io/syndesis/connector/sql/SqlStartConnectorComponent.java
+++ app/connectors/connectors/sql/sql-start-connector/src/main/java/io/syndesis/connector/sql/SqlStartConnectorComponent.java
@@ -16,35 +16,34 @@
  */
 package io.syndesis.connector.sql;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import io.syndesis.connector.sql.stored.JSONBeanUtil;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Consumer;
 import org.apache.camel.Endpoint;
+import org.apache.camel.EndpointConfiguration;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
 import org.apache.camel.Expression;
+import org.apache.camel.PollingConsumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.builder.ExpressionBuilder;
-import org.apache.camel.builder.ValueBuilder;
 import org.apache.camel.component.connector.DefaultConnectorComponent;
-import org.apache.camel.processor.ChoiceProcessor;
-import org.apache.camel.processor.FilterProcessor;
-import org.apache.camel.processor.Pipeline;
+import org.apache.camel.component.connector.SchedulerTimerConnectorEndpoint;
 import org.apache.camel.processor.Splitter;
-import org.apache.camel.processor.aggregate.AggregationStrategy;
 
-import io.syndesis.connector.sql.stored.JSONBeanUtil;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * Camel SqlConnector connector
  */
 public class SqlStartConnectorComponent extends DefaultConnectorComponent {
 
-    final static String COMPONENT_NAME  ="sql-start-connector";
-    final static String COMPONENT_SCHEME="sql-start-connector";
+    final static String COMPONENT_NAME = "sql-start-connector";
+    final static String COMPONENT_SCHEME = "sql-start-connector";
 
     public SqlStartConnectorComponent() {
         super(COMPONENT_NAME, SqlStartConnectorComponent.class.getName());
@@ -52,29 +51,27 @@ public class SqlStartConnectorComponent extends DefaultConnectorComponent {
         registerExtension(SqlConnectorMetaDataExtension::new);
     }
 
-    public SqlStartConnectorComponent(String componentScheme) {
+    // @Override
+    // public Endpoint createEndpoint(String uri) throws Exception {
+    // // TODO Auto-generated method stub
+    // Endpoint endpoint = super.createEndpoint(uri);
+    // Producer producer = endpoint.createProducer();
+    //
+    //
+    //
+    // return endpoint;
+    // }
+
+    public SqlStartConnectorComponent(final String componentScheme) {
         super(COMPONENT_NAME, SqlStartConnectorComponent.class.getName());
     }
 
-//    @Override
-//    public Endpoint createEndpoint(String uri) throws Exception {
-//        // TODO Auto-generated method stub
-//        Endpoint endpoint = super.createEndpoint(uri);
-//        Producer producer = endpoint.createProducer();
-//        
-//        
-//        
-//        return endpoint;
-//    }
-    
-    
-    
     @Override
     public Processor getBeforeProducer() {
 
         final Processor processor = exchange -> {
             final String body = (String) exchange.getIn().getBody();
-            if (body!=null) {
+            if (body != null) {
                 final Properties properties = JSONBeanUtil.parsePropertiesFromJSONBean(body);
                 exchange.getIn().setBody(properties);
             }
@@ -83,40 +80,96 @@ public class SqlStartConnectorComponent extends DefaultConnectorComponent {
     }
 
     @Override
-    public Processor getAfterProducer() {
-        @SuppressWarnings("unchecked")
-        List<String> jsonList = new ArrayList<>();
-        final Processor processor = exchange -> {
-            
-            if (exchange.getIn().getBody(List.class) != null) {
-                List<Map> list = exchange.getIn().getBody(List.class);
-                for (Map map : list) {
-                    //String jsonBean = JSONBeanUtil.toJSONBean(map);
-                    List<String> names = exchange.getContext().getComponentNames();
-                    Collection endpoints = exchange.getContext().getEndpoints();
-                    //exchange.getContext().createProducerTemplate().sendBody("stream:out",jsonBean);
-                    //exchange.setProperty(Exchange.ROUTE_STOP, Boolean.TRUE); 
-                }
+    protected Endpoint createEndpoint(final String uri, final String remaining, final Map<String, Object> parameters) throws Exception {
+        final SchedulerTimerConnectorEndpoint endpoint = (SchedulerTimerConnectorEndpoint) super.createEndpoint(uri, remaining, parameters);
+
+        return new Endpoint() {
+
+            @Override
+            public void configureProperties(final Map<String, Object> options) {
+                endpoint.configureProperties(options);
             }
-//            } else {
-//            if (exchange.getIn().getBody().getClass().equals(Map.class)) {
-//                String jsonBean = JSONBeanUtil.toJSONBean(exchange.getIn().getBody(Map.class));
-//                exchange.getIn().setBody("JSON");
-//                
-//                exchange.getContext().createProducerTemplate().sendBody(jsonBean);
-//                exchange.setProperty(Exchange.ROUTE_STOP, Boolean.TRUE); 
-//            }
-            
+
+            @Override
+            public Consumer createConsumer(final Processor processor) throws Exception {
+                final Expression expression = ExpressionBuilder.bodyExpression(List.class);
+                final Splitter splitter = new Splitter(getCamelContext(), expression, processor, null);
+
+                endpoint.setAfterProducer(splitter);
+
+                return endpoint.createConsumer(processor);
+            }
+
+            @Override
+            public Exchange createExchange() {
+                return endpoint.createExchange();
+            }
+
+            @Override
+            public Exchange createExchange(final Exchange exchange) {
+                return endpoint.createExchange(exchange);
+            }
+
+            @Override
+            public Exchange createExchange(final ExchangePattern pattern) {
+                return endpoint.createExchange(pattern);
+            }
+
+            @Override
+            public PollingConsumer createPollingConsumer() throws Exception {
+                return endpoint.createPollingConsumer();
+            }
+
+            @Override
+            public Producer createProducer() throws Exception {
+                return endpoint.createProducer();
+            }
+
+            @Override
+            public CamelContext getCamelContext() {
+                return endpoint.getCamelContext();
+            }
+
+            @Override
+            public EndpointConfiguration getEndpointConfiguration() {
+                return endpoint.getEndpointConfiguration();
+            }
+
+            @Override
+            public String getEndpointKey() {
+                return endpoint.getEndpointKey();
+            }
+
+            @Override
+            public String getEndpointUri() {
+                return endpoint.getEndpointUri();
+            }
+
+            @Override
+            public boolean isLenientProperties() {
+                return endpoint.isLenientProperties();
+            }
+
+            @Override
+            public boolean isSingleton() {
+                return endpoint.isSingleton();
+            }
+
+            @Override
+            public void setCamelContext(final CamelContext context) {
+                endpoint.setCamelContext(context);
+            }
+
+            @Override
+            public void start() throws Exception {
+                endpoint.start();
+            }
+
+            @Override
+            public void stop() throws Exception {
+                endpoint.stop();
+            }
         };
-        
-        //FilterProcessor choice = new FilterProcessor(predicate, processor)
-            Expression expression = ExpressionBuilder.bodyExpression(List.class);
-            Splitter splitter = new Splitter(getCamelContext(), expression, processor, null);
-            final Processor pipeline = Pipeline.newInstance(getCamelContext(), splitter);
-        
-            return pipeline;
-        
-        
     }
 
 }
